bbf635c89ba5a4622a8c957d6bb814cd8df464c7
Olenka Sanchez <olesa600@gmail.com>
2023-01-31 16:29:52 -0500

    ADD: add styles file

---
+++ nttdata-challenge/src/styles/colors.js
@@ -0,0 +1,25 @@
+export const colors = {
+  gray: {
+    400: "#B2B6C4",
+    600: "#677294",
+    900: "#1B1E29",
+  },
+  green: "#44C566",
+
+  blue: "#051441",
+
+  lightBlue: "#007BFF",
+
+  orange: "#FF5B00",
+
+  red: "#EF4444",
+
+  teal: "#31B9DD",
+
+  yellow: "#FBBF24",
+
+  black: "#171717",
+  white: "#FFFFFF",
+
+  background: "rgba(230, 232, 235, 0.31)",
+};
