b834dde6b57be8e2c3df0a3afd0bfafdb580b964
Olenka Sanchez <olesa600@gmail.com>
2023-01-31 16:34:11 -0500

    WIP: bring 5 users information

--- nttdata-challenge/src/App.js
+++ nttdata-challenge/src/App.js
@@ -1,5 +1,16 @@
+import { useEffect } from "react";
+import { getAllUsers } from "./service/api-fetch"
 
 function App() {
+  useEffect(() => {
+    getAllUsers()
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [])
   return (
     <div>
       <h1>Holaa</h1>
